965ffae13b1b63a90e9ce4aa7a1bebaa0a942481
Junior DUSSOUILLEZ <junior.dussouillez@gmail.com>
2022-11-05 15:09:11 +0100

    fix: fix extra spaces

--- .bashrc_custom.sh
+++ .bashrc_custom.sh
@@ -140,8 +140,8 @@ complete -F _completemarks jump j unmark umark
 # Node #
 ########
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 ###########
 # Network #
