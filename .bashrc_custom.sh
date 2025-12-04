c5503ea6e44ea6a313bdc3c0af1bacf78277411d
Junior Dussouillez <5428355+jdussouillez@users.noreply.github.com>
2025-12-04 08:59:24 +0100

    Update .bashrc_custom.sh
    docs: remove deprecated warning

--- .bashrc_custom.sh
+++ .bashrc_custom.sh
@@ -14,7 +14,6 @@ alias json="bat -l json"
 alias yaml="bat -l yaml"
 # alias xed="gnome-text-editor" # Ubuntu only
 
-# Deprecated, use "topgrade" instead
 update() {
     set -x
     sudo apt-get update
